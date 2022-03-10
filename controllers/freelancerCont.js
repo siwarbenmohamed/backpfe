b4ab5a74c78ff5a1b2603e3593d4c2b673469b0e
siwar ben mohamed <siwarbenmohamed120@gmail.com>
2022-03-10 12:04:36 +0100

    send email

--- controllers/freelancerCont.js
+++ controllers/freelancerCont.js
@@ -1,36 +1,72 @@
 const Freelancer = require("../models/freelancer");
 const bcrypt = require("bcryptjs")
+const nodemailer = require("nodemailer");
+
+var transporter = nodemailer.createTransport({
+  service: "Gmail",
+  auth: {
+    user: "freelancertunisie11@gmail.com",
+    pass: "Freelance123."
+  },
+});
+
 
 exports.saveFreelancer = async (req, res, next) => {
 
-  //hashing the password
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(req.body.password, salt);
-  const freelancer = new Freelancer({
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    cin: req.body.cin,
-    tel: req.body.tel,
-    email: req.body.email,
-    password: hashedPassword
+  try {
+    
+      //checking if user email already exixts
+      const emailExist = await Freelancer.findOne({ email: req.body.email });
+      if (emailExist) {
+          res.status(400).send("Email already exists");
+          return;
+      }
+
+    //hashing the password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    const freelancer = new Freelancer({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      cin: req.body.cin,
+      tel: req.body.tel,
+      email: req.body.email,
+      password: hashedPassword
 
 
-  });
-  freelancer
-    .save()
-    .then(result => {
-      console.log(result);
-      res.status(200).json({
-        status: true,
-        data: freelancer,
-      });
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json({
-        error: err
-      });
     });
+    freelancer
+      .save()
+      .then(result => {
+        console.log(result);
+        var mailOptions = {
+          from: "siwarbenmohamed120@gmail.com",
+          to: req.body.email,
+          subject: `Freelancer`,
+          html: `<h2>${freelancer.firstName}! Thanks for registering on our site</h2>`
+
+        };
+
+        transporter.sendMail(mailOptions, function (error, info) {
+          if (error) {
+            console.log(error);
+            res.send("error"); // if error occurs send error as response to client
+          } else {
+            console.log("Email sent: " + info.response);
+            res.send("Sent Successfully"); //if mail is sent successfully send Sent successfully as response
+          }
+        });
+        console.log("sent")
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json({
+          error: err
+        });
+      });
+  } catch (err) {
+    console.log(err)
+  }
 };
 
 exports.get = (req, res) => {
